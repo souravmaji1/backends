3c95bb909bc4c8342fbbe40d01fb588aa3f37acb
sourav maji <majisourav255@gmail.com>
2023-12-30 16:03:27 +0530

    updated

--- src/app/components/deposit/breeding.js
+++ src/app/components/deposit/breeding.js
@@ -1,106 +1,125 @@
-// components/BreedingComponent.js
-import React, { useState } from 'react';
-import { client } from "@/app/client";
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import Image from 'next/image';
-import DuckyOne from '../../../../public/assets/img/ducky.jpg';
-import DuckyTwo from '../../../../public/assets/img/ducky.jpg';
-import DuckyThree from '../../../../public/assets/img/ducky.jpg';
-import DuckyFour from '../../../../public/assets/img/ducky.jpg';
+'use client'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useCookies } from "react-cookie";
 
-const BreedingComponent = () => {
-  const [parents, setParents] = useState([
-    { id: 1, image: DuckyOne, selected: false },
-    { id: 2, image: DuckyTwo, selected: false },
-    { id: 3, image: DuckyThree, selected: false },
-    { id: 4, image: DuckyFour, selected: false },
-  ]);
+export default function BreedPage() {
+  const [duckCount, setDuckCount] = useState(null);
+  const [males, setMales] = useState([]);
+  const [females, setFemales] = useState([]);
+  const [selectedMale, setSelectedMale] = useState(null);
+  const [selectedFemale, setSelectedFemale] = useState(null);
+  const [cookie] = useCookies(["token"]);
 
-  const [breedingDate, setBreedingDate] = useState(new Date());
-  const [responseMessage, setResponseMessage] = useState('');
+  useEffect(() => {
+    const fetchDucks = async () => {
+      try {
+        const response = await axios.get('/duck/duck-count', {
+          headers: {
+            Authorization: `Bearer ${cookie.token}`,
+          },
+        });
 
-  const handleParentClick = (id) => {
-    const updatedParents = parents.map((parent) =>
-      parent.id === id ? { ...parent, selected: !parent.selected } : parent
-    );
-    setParents(updatedParents);
+        setDuckCount(response.data.duckCount);
+
+        if (response.data.duckCount >= 2) {
+          const ducksResponse = await axios.get('/duck/ducks', {
+            headers: {
+              Authorization: `Bearer ${cookie.token}`,
+            },
+          });
+
+          const maleDucks = ducksResponse.data.filter((duck) => duck.gender === 'MALE');
+          const femaleDucks = ducksResponse.data.filter((duck) => duck.gender === 'FEMALE');
+
+          setMales(maleDucks);
+          setFemales(femaleDucks);
+        }
+      } catch (error) {
+        console.error('Error fetching duck count:', error.message);
+      }
+    };
+
+    fetchDucks();
+  }, []);
+
+  const handleMaleSelect = (duck) => {
+    setSelectedMale(duck);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleFemaleSelect = (duck) => {
+    setSelectedFemale(duck);
+  };
 
-    const selectedParents = parents.filter((parent) => parent.selected);
-
-    if (selectedParents.length !== 2) {
-      setResponseMessage('Please select exactly two parents.');
-      return;
-    }
-
-    const parent1 = selectedParents[0];
-    const parent2 = selectedParents[1];
-
+  const handleBreedClick = async () => {
     try {
-      const response = await client.post('/api/breedingEndpoint', {
-        parent1,
-        parent2,
-        breedingDate,
+      const response = await axios.post('/duck/breed', {
+        male_id: selectedMale.id,
+        female_id: selectedFemale.id,
+      }, {
+        headers: {
+          Authorization: `Bearer ${cookie.token}`,
+        },
       });
 
-      setResponseMessage(response.data.message);
+      console.log('Breeding success:', response.data);
     } catch (error) {
-      setResponseMessage('Error occurred while making the request.');
-      console.error('Error:', error);
+      console.error('Error breeding ducks:', error.message);
     }
   };
 
   return (
-    <div className="inline-table mx-auto mt-8 p-8 bg-dark_light rounded shadow-md flex">
-      <div className="max-w-2xl mx-auto mt-8 p-6 rounded-md shadow-md" style={{ background: '#6820C2' }}>
-        <h2 className="text-2xl font-semibold mb-4 text-white">Breeding Ducks</h2>
-
-        <div className="grid grid-cols-2 gap-8">
-          {parents.map((parent) => (
-            <div
-              key={parent.id}
-              className={`bg-white rounded-md p-4 cursor-pointer ${
-                parent.selected ? 'border-4 border-blue-500' : ''
-              }`}
-              onClick={() => handleParentClick(parent.id)}
-            >
-           {/*   <h3 className="text-lg font-semibold mb-2">{`Parent ${parent.id}`}</h3>*/}
-              <Image src={parent.image} alt={`Parent ${parent.id}`} />
+    <div className="container mx-auto p-8">
+      <h1 className="text-3xl font-bold mb-6">Breeding Page</h1>
+      {duckCount !== null ? (
+        duckCount >= 2 ? (
+          <div>
+            <p className="mb-4">Your duck count: {duckCount}</p>
+            <div className="flex justify-between">
+              <div className="w-1/2">
+                <h2 className="text-xl font-bold mb-4">Select Male Duck</h2>
+                <ul>
+                  {males.map((duck) => (
+                    <li
+                      key={duck.id}
+                      onClick={() => handleMaleSelect(duck)}
+                      className={`cursor-pointer p-2 rounded ${selectedMale && selectedMale.id === duck.id ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}
+                    >
+                      {duck.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="w-1/2">
+                <h2 className="text-xl font-bold mb-4">Select Female Duck</h2>
+                <ul>
+                  {females.map((duck) => (
+                    <li
+                      key={duck.id}
+                      onClick={() => handleFemaleSelect(duck)}
+                      className={`cursor-pointer p-2 rounded ${selectedFemale && selectedFemale.id === duck.id ? 'bg-pink-500 text-white' : 'bg-gray-200'}`}
+                    >
+                      {duck.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-          ))}
-        </div>
-
-        <form onSubmit={handleSubmit}>
-          {/* ... (existing form fields) */}
-
-          <br></br>
-
-          {/* Breeding Date Input */}
-          <div className="mb-4">
-            <label className="block text-sm font-medium text-white">Breeding Date:</label>
-            <DatePicker
-              selected={breedingDate}
-              onChange={(date) => setBreedingDate(date)}
-              className="mt-1 p-2 w-full border rounded-md"
-            />
+            {selectedMale && selectedFemale && (
+              <button
+                onClick={handleBreedClick}
+                className="mt-6 bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600 cursor-pointer"
+              >
+                Start Breeding
+              </button>
+            )}
           </div>
-
-          <button
-            type="submit"
-            style={{ background: '#222242' }}
-            className="text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-          >
-            Start Breeding
-          </button>
-        </form>
-        {responseMessage && <p className="mt-4 text-green-600">{responseMessage}</p>}
-      </div>
+        ) : (
+          <p>You need at least 2 ducks to start breeding.</p>
+        )
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
-};
-
-export default BreedingComponent;
+}
